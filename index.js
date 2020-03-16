ea8429e2ccdc7c018d187ad5e4459a45534b60c7
ITVillage-Kevin <it.village.host@gmail.com>
2020-03-16 12:34:48 +0900

    modifying index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    $("#btnStart").click(function(){
+        function getWeatherData() {
+            var eventSource = new EventSource("http://localhost:8080/stream/weather");
+            eventSource.onmessage = e => {
+                var result = JSON.parse(e.data);
+                console.log(result);
+            }
+        }
 
+        getWeatherData();
+    });
 })
